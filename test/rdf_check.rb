6d5fd01d04673b6f2bcfa242f71988b3bc1a0b03
gebele <gebele@in-silico.ch>
2012-05-07 11:28:40 +0200

    update valid test data/add rdf_check

--- test/rdf_check.rb
+++ test/rdf_check.rb
@@ -11,17 +11,19 @@ class UploadTest < Test::Unit::TestCase
   def test_01_check_rdf_with_ToxBank_specific_fields_on_BII_I_1
     id = OpenTox::Authorization.authenticate($aa[:user],$aa[:password])
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
+    assert_match /[Term Source Name, OBI, DOID, BTO, NEWT, UO, CHEBI, PATO, TBP, TBC, TBO, TBU, TBK]/, response
+    response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
     assert_match /[Investigation Identifier, BII\-I\-1]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
     assert_match /[Investigation Title, Growth control of the eukaryote cell\: a systems biology study in yeast]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
     assert_match /[Investigation Description, Background Cell growth underlies many key cellular and developmental processes]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
-    assert_match /[Owning Organisation URI, TBO\:G176]/, response
-    #test_id_query_sparqle
+    assert_match /[Owning Organisation URI, TBO\:G176, 	Public]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
-    assert_match /[Consortium URI, TBC:G2]/, response
-    #test_resource_ISA
+    assert_match /[Consortium URI, TBC\:G2, Douglas Connect]/, response
+    response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
+    assert_match /[Principal Investigator URI, TBU\:U115, Glenn	Myatt]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/0`.chomp
     assert_match /[Investigation keywords, TBK\:Blotting, Southwestern;TBK\:Molecular Imaging;DOID\:primary carcinoma of the liver cells]/, response
   end
@@ -35,11 +37,9 @@ class UploadTest < Test::Unit::TestCase
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/2`.chomp
     assert_match /[Investigation Description, The Toxicogenomics Project was a 5\-year collaborative project]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/2`.chomp
-    assert_match /[Owning Organisation URI, TBO\:G176]/, response
-    #test_id_query_sparqle
+    assert_match /[Owning Organisation URI, TBO\:G176, Public]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/2`.chomp
-    assert_match /[Consortium URI, TBC:G2]/, response
-    #test_resource_ISA
+    assert_match /[Consortium URI, TBC:G2, Douglas Connect]/, response
     response = `curl -i -k -H subjectid:#{id} -H accept:application/rdf+xml https://toxbank-dev.in-silico.ch/2`.chomp
     assert_match /[Investigation keywords, TBK\:Fluxomics]/, response
   end
