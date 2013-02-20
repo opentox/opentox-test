ee5d920039919263be0602ba66c6e65c85e9cfb1
rautenberg <rautenberg@in-silico.ch>
2013-02-20 10:32:07 +0100

    add tests to investigation publish/policy/search

--- test/toxbank_investigation_AA.rb
+++ test/toxbank_investigation_AA.rb
@@ -22,6 +22,7 @@ class TBInvestigationAA < Test::Unit::TestCase
 # Download        y      n      n      y      n      n      n
 #
   # create a new investigation by uploading a zip file,
+  # owner is $pi, Summary is not searchable, access=custom(owner only), not published
   def test_01_post_investigation
     @@uri = ""
     file = File.join File.dirname(__FILE__), "data/toxbank-investigation/valid", "BII-I-1b-tb2.zip"
@@ -33,6 +34,24 @@ class TBInvestigationAA < Test::Unit::TestCase
     assert_equal "Completed", task.hasStatus, "Task should be completed but is: #{task.hasStatus}. Task URI is #{task_uri} ."
     @@uri = URI(uri)
   end
+
+  # check if @@uri is not in search-index
+  def test_02_investigation_not_in_searchindex
+    response = OpenTox::RestClientWrapper.get "#{$search_service[:uri]}/search/index/investigation?resourceUri=#{CGI.escape(@@uri.to_s)}",{},{:subjectid => $pi[:subjectid]}
+    assert_equal 200, response.code
+    assert_no_match /#{@@uri}/, response.to_s
+  end
+
+  # check for flag "isPublished" is false,
+  # @note default behaviour on new investigations
+  def test_03_check_published_false
+    data = OpenTox::RestClientWrapper.get "#{@@uri}/metadata", {}, {:accept => "application/rdf+xml", :subjectid => $pi[:subjectid]}
+    @g = RDF::Graph.new
+    RDF::Reader.for(:rdfxml).new(data.to_s){|r| r.each{|s| @g << s}}
+    @g.query(:predicate => RDF::TB.isPublished){|r| assert_match r[2].to_s, /false/}
+  end
+
+
 
   # delete investigation/{id}
   # @note expect code 200
