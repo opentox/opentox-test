023737f030fe7b205381dcc169e0737da7f66261
davor <vorgrimmlerdavid@gmx.de>
2013-05-29 10:30:19 +0200

    Added bbrc lazar test with modified min_frequency

--- test/lazar-fminer.rb
+++ test/lazar-fminer.rb
@@ -54,7 +54,31 @@ class FminerLazarTest < MiniTest::Test
     prediction = OpenTox::Dataset.new prediction_uri, @@subjectid
     assert_equal prediction.uri.uri?, true
   end
-=begin
-=end
 
+  def test_05_lazar_bbrc_ham_minfreq
+    dataset = OpenTox::Dataset.new nil, @@subjectid
+    dataset.upload File.join(DATA_DIR,"hamster_carcinogenicity.csv")
+    assert_equal dataset.uri.uri?, true
+    lazar = OpenTox::Algorithm.new File.join($algorithm[:uri],"lazar"), @@subjectid
+    model_uri = lazar.run :dataset_uri => dataset.uri, :feature_generation_uri => File.join($algorithm[:uri],"fminer","bbrc"), :min_frequency => 5
+    assert_equal model_uri.uri?, true
+    model = OpenTox::Model.new model_uri, @@subjectid
+    assert_equal model.uri.uri?, true
+    feature_dataset_uri = model[RDF::OT.featureDataset]
+    feature_dataset = OpenTox::Dataset.new feature_dataset_uri , @@subjectid
+    assert_equal dataset.compounds.size, feature_dataset.compounds.size
+    assert_equal 41, feature_dataset.features.size
+    assert_equal '[#7&A]-[#6&A]=[#7&A]', OpenTox::Feature.new(feature_dataset.features.first.uri, @@subjectid).title
+    compound = OpenTox::Compound.from_inchi("InChI=1S/C6H6/c1-2-4-6-5-3-1/h1-6H")
+    prediction_uri = model.run :compound_uri => compound.uri
+    prediction_dataset = OpenTox::Dataset.new prediction_uri, @@subjectid
+    assert_equal prediction_dataset.uri.uri?, true
+    prediction = prediction_dataset.predictions.select{|p| p[:compound].uri == compound.uri}.first
+    assert_equal "false", prediction[:value]
+    assert_equal 0.12380952380952381, prediction[:confidence]
+    dataset.delete
+    model.delete
+    feature_dataset.delete
+    prediction_dataset.delete
+  end
 end
