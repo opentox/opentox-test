c7ffa17f79cc9c06d9bf781b02e29fe21bb67ffd
gebele <gebele@in-silico.ch>
2013-06-12 19:25:45 +0200

    added urilist test

---
+++ test/urilist.rb
@@ -0,0 +1,23 @@
+require_relative "setup.rb"
+
+class UriListTest < MiniTest::Test
+
+  def test_01_urilist_dublicates
+    services = {1=>"algorithm", 3=>"dataset", 4=>"feature", 5=>"model", 6=>"task"}
+    services.each do |k, service|
+      s_urilist = `curl -H accept:text/uri-list http://localhost:808#{k}/#{service}`.split("\n") 
+      assert_equal s_urilist.uniq.length, s_urilist.length, "Attention, dublicates found in #{service} uri-list!"
+    end
+  end
+
+  def test_02_urilist_mime
+    mime_types = ["application/rdf+xml", "text/turtle", "application/sparql-results+xml", "text/plain", "text/uri-list", "text/html"]
+    mime_types.each do |mt|
+      s_urilist = `curl -i -H accept:#{mt} http://localhost:8084/feature`
+      assert_match /200 OK/, s_urilist.to_s
+      refute_match /Content-Length: 0/, s_urilist.to_s
+      refute_match /dataset/, s_urilist.to_s
+    end
+  end
+
+end
