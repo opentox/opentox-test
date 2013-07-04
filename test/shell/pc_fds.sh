072c34deefad5208852ca24cf9f60f3f40088e10
Christoph Helma <helma@in-silico.ch>
2013-07-04 18:18:34 +0200

    pc-lazar.rb and shell tests removed

--- test/shell/pc_fds.sh
+++
@@ -1,9 +0,0 @@
-dir=`dirname -z "$0"`
-. $dir/setup.sh
-
-task=`curl \
---data-urlencode "pc_type=geometrical"  \
--X POST $ds/pc`
-echo $task
-pc_fds=`get_result "$task"`
-echo "pc_fds: $pc_fds"
--- test/shell/setup.sh
+++
@@ -1,9 +0,0 @@
-# util 
-function get_result {
-  task=$1
-  while [ 1 ]; do
-    result=`curl -H "accept:text/uri-list" $task 2>/dev/null`
-    [ $result == $task ] && sleep 1 || break
-  done
-  echo $result
-}
