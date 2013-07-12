864bf3be4e045740267ecc3f530ef43f41e4574f
Christoph Helma <helma@in-silico.ch>
2013-07-12 18:24:49 +0200

    unsupported mime-types removed from uri-list tests

--- test/urilist.rb
+++ test/urilist.rb
@@ -11,9 +11,12 @@ class UriListTest < MiniTest::Test
   end
 
   def test_02_urilist_mime
-    mime_types = ["application/rdf+xml", "text/turtle", "application/sparql-results+xml", "text/plain", "text/uri-list", "text/html"]
+    # "application/rdf+xml", "text/turtle", "application/sparql-results+xml" are currently not supported and might lead to performance problem. I guess it is not worth the efforts to implement them at the moment, text/uri-list should be sufficient for most purposes (CH)
+    #mime_types = ["application/rdf+xml", "text/turtle", "application/sparql-results+xml", "text/plain", "text/uri-list", "text/html"]
+    mime_types = [ "text/plain","text/uri-list", "text/html"]
     mime_types.each do |mt|
       s_urilist = `curl -ik -H accept:#{mt} #{$feature[:uri]}`
+      #puts s_urilist
       assert_match /200 OK/, s_urilist.to_s
       refute_match /Content-Length: 0/, s_urilist.to_s, "Attention, content length empty!"
       refute_match /dataset/, s_urilist.to_s, "Attention, found dataset in feature uri-list!"
