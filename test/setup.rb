fad1d4b322958e1c64837bf9311afc7f59ca0871
rautenberg <rautenberg@in-silico.ch>
2013-07-25 09:38:35 +0200

    set OpenTox::ReatClientWrapper.subjectid in setup

--- test/setup.rb
+++ test/setup.rb
@@ -8,8 +8,6 @@ TEST_DIR ||= File.expand_path(File.dirname(__FILE__))
 DATA_DIR ||= File.join(TEST_DIR,"data")
 
 unless $aa[:uri].to_s == ""
-  $pi[:subjectid] = OpenTox::Authorization.authenticate($pi[:name], $pi[:password])
-  $secondpi[:subjectid] = OpenTox::Authorization.authenticate($secondpi[:name], $secondpi[:password])
-  unauthorized_error "Failed to authenticate user \"#{$aa[:user]}\"." unless OpenTox::Authorization.is_token_valid $pi[:subjectid]
-  unauthorized_error "Failed to authenticate user \"#{$aa[:user]}\"." unless OpenTox::Authorization.is_token_valid $secondpi[:subjectid]
+  OpenTox::RestClientWrapper.subjectid = OpenTox::Authorization.authenticate($aa[:user], $aa[:password])
+  unauthorized_error "Failed to authenticate user \"#{$pi[:user]}\"." unless OpenTox::Authorization.is_token_valid OpenTox::RestClientWrapper.subjectid
 end
