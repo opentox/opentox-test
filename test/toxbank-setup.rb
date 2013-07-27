5da7653c8a4209f0623454567b527465b662483d
rautenberg <rautenberg@in-silico.ch>
2013-07-27 23:06:07 +0200

    fix toxbank-setup authentication

--- test/toxbank-setup.rb
+++ test/toxbank-setup.rb
@@ -1,7 +1,10 @@
 require_relative "setup.rb"
 unless $aa[:uri].to_s == ""
-  $pi[:subjectid] = OpenTox::Authorization.authenticate($pi[:name], $pi[:password])
-  $secondpi[:subjectid] = OpenTox::Authorization.authenticate($secondpi[:name], $secondpi[:password])
+  OpenTox::Authorization.authenticate($pi[:name], $pi[:password])
+  $pi[:subjectid] = OpenTox::RestClientWrapper.subjectid
+  OpenTox::Authorization.authenticate($secondpi[:name], $secondpi[:password]) 
+  $secondpi[:subjectid] = OpenTox::RestClientWrapper.subjectid
   unauthorized_error "Failed to authenticate user \"#{$aa[:user]}\"." unless OpenTox::Authorization.is_token_valid $pi[:subjectid]
   unauthorized_error "Failed to authenticate user \"#{$aa[:user]}\"." unless OpenTox::Authorization.is_token_valid $secondpi[:subjectid]
+  OpenTox::Authorization.authenticate($aa[:user], $aa[:password])
 end
