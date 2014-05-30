910310a0b23c5572b7c6c5005a51adb9c8eb9e3a
rautenberg <rautenberg@in-silico.ch>
2014-05-30 13:04:04 +0200

    split some tests for better overview

--- test/toxbank-investigation-ftp.rb
+++ test/toxbank-investigation-ftp.rb
@@ -14,6 +14,7 @@ class TBInvestigationFTP < MiniTest::Test
 
   i_suck_and_my_tests_are_order_dependent!
   
+  $testfile = File.join File.dirname(__FILE__), "data/toxbank-investigation/valid", "unformated.zip"
   $testdir = "nightlytempdir#{Time.now.strftime("%Y%m%d")}" # test directory on ftp server e.G.: nightlytempdir20140528
 
   # check user root dir
@@ -22,10 +23,8 @@ class TBInvestigationFTP < MiniTest::Test
     assert_equal $ftp.pwd, "/"
   end
 
-  # create a testdirectory and upload a file 
+  # create a testdirectory
   def test_01_create_folder_and_upload_file
-    file = File.join File.dirname(__FILE__), "data/toxbank-investigation/valid", "unformated.zip"
-    
     begin
       $ftp.chdir($testdir)
     rescue Net::FTPPermError, NameError => boom
@@ -34,23 +33,30 @@ class TBInvestigationFTP < MiniTest::Test
     $ftp.chdir("/")
     $ftp.chdir($testdir)
     assert_equal $ftp.pwd, "/#{$testdir}"
-    files = $ftp.list
-    
-    $ftp.putbinaryfile(file)
-    f = $ftp.list(File.basename(file))
-    assert_equal f[0].split[8], File.basename(file), "file #{File.basename(file)} do not exist on ftp server." 
+  end
 
+  # upload testfile
+  def test_02_upload_file
+    $ftp.putbinaryfile($testfile)
+    f = $ftp.list(File.basename($testfile))
+    assert_equal f[0].split[8], File.basename($testfile), "file #{File.basename($testfile)} do not exist on ftp server."
+  end
+
+  # check http://api.toxbank.net/index.php/Investigation#Get_a_list_of_uploaded_FTP_files
+  def test_03_check_ftpfiles_urilist
     response = OpenTox::RestClientWrapper.get  $investigation[:uri]+"/ftpfiles", {}, {:accept => "text/uri-list", :subjectid => $pi[:subjectid] }
     assert_equal "200", response.code.to_s
-
-    files_to_check = ["subdir/JIC37_Ethanol_0.07_Internal_1_3.txt","JIC37_Ethanol_0.07_Internal_1_3.txt","subdir/isttest.txt","isttest.txt","#{$testdir}/#{File.basename(file)}"]
+    files_to_check = ["subdir/JIC37_Ethanol_0.07_Internal_1_3.txt","JIC37_Ethanol_0.07_Internal_1_3.txt","subdir/isttest.txt","isttest.txt","#{$testdir}/#{File.basename($testfile)}"]
     files_to_check.each do |ftc|
       refute_nil response.match("(^|\n)#{ftc}(\n|$)"), "File: #{ftc} is not in ftpfiles"
     end
+  end
 
-    $ftp.delete(File.basename(file))
+  # delete file from test_02
+  def test_04_delete_file
+    $ftp.delete(File.basename($testfile))
     assert_raises Net::FTPTempError do
-      f = $ftp.list(File.basename(file))
+      f = $ftp.list(File.basename($testfile))
     end
     $ftp.chdir("/")      
     $ftp.rmdir($testdir)
@@ -58,7 +64,6 @@ class TBInvestigationFTP < MiniTest::Test
       $ftp.chdir($testdir)
     end
   end
-
 
   # close connection and check if it is closed
   def test_99_close
