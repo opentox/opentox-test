7d907246a1277545b0aa910713dc24e94fccefc7
davor <vorgrimmlerdavid@gmx.de>
2014-08-26 17:02:24 +0200

    Excluded aop tests.

--- opentox-client-all.rb
+++ opentox-client-all.rb
@@ -2,7 +2,9 @@ require 'minitest/autorun'
 require 'openbabel'
 all = Dir["test/*.rb"]
 exclude = [
-  "test/*setup.rb",
   "test/lazarweb.rb",
+  "test/*setup.rb",
+  "test/aop-curl.rb",
+  "test/aopweb.rb",
 ] + Dir["test/toxbank*.rb"]
 (all - exclude).each {|f| require_relative f }
