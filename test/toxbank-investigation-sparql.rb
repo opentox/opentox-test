897b77b7760ddfe9966177357e878e26673e24a8
gebele <gebele@in-silico.ch>
2014-10-24 16:56:37 +0200

    put task status

--- test/toxbank-investigation-sparql.rb
+++ test/toxbank-investigation-sparql.rb
@@ -239,9 +239,10 @@ class TBSPARQLTestExtended < MiniTest::Test
     if subtask != ""
       task = OpenTox::Task.new subtask.chomp
       task.wait
+      puts task.hasStatus
     end
-    puts "sleep 120s"
-    sleep 120
+    #puts "sleep 120s"
+    #sleep 120
   end  
   
   # Retrieves all factors (name, value, ontology URI of the value) given an investigation URI
